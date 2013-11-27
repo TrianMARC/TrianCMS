a516feb99345b7e2beb68f806c89bd1dc5075b90
TrianMARC <victor.sanchez5@alu.uclm.es>
2013-11-28 00:01:50 +0100

    rev 0.0.7

--- secciones/admin/inicio.php
+++ secciones/admin/inicio.php
@@ -4,17 +4,21 @@ if (!defined('Verificado'))
     die("Acceso no permitido");
 
 function principal(){
-	global $es_user,$seccion,$tu_cuenta;
+	global $es_user,$seccion,$tu_cuenta,$es_admin;
 	if(!$es_user){
 		header('Location: ./?seccion=users');
 	}
 	else {
-	 
-		$nick = $_COOKIE['user'];
-		$cont = [
-				 'nick' => $nick,
-				];
-		plantilla(incluir_html($cont,'inicio'));		
+		if(!$es_admin){
+			header('Location: ./');
+		}
+		else{
+			$nick = $tu_cuenta['usuario'];
+			$cont = [
+					'nick' => $nick,
+					];	
+			plantilla(incluir_html($cont,'inicio'));
+		}
 	 }
 }
 
